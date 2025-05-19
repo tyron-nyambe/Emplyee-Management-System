5d635f8f1d101cc12f513ad9187744ca99e2a4bc
Tyron <tyronnyambe@gmail.com>
2025-05-19 21:58:57 +0200

    Add frontend project to repo

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All Rights Reserved 2025 by NSolutions</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
---
+++ ems-frontend/src/components/ListEmployeeComponent.jsx
@@ -0,0 +1,110 @@
+import React,{useEffect, useState} from 'react'
+import { deleteEmployee, listEmployees } from '../services/EmployeeService'
+import { useNavigate } from 'react-router-dom'
+
+const ListEmployeeComponent = () => {
+
+/*
+    const dummyData = [
+
+    {
+        "id": 1,
+        "firstName": "Small",
+        "lastName": "James",
+        "email": "smalljames@gmail.com"
+    },
+    {
+        "id":2,
+        "firstName": "Simon",
+        "lastName": "Peter",
+        "email": "simonpeter@gmail.com"
+    },
+    {
+        "id":3,
+        "firstName": "Big",
+        "lastName": "James",
+        "email": "bigjames@gmail.com"
+    }
+]
+    */
+
+    const [employees,setEmployees] = useState([])
+
+    const navigator = useNavigate();
+
+    function getAllEmployees(){
+        listEmployees().then((response) =>{
+            setEmployees(response.data)
+        }).catch(error => {
+            console.error(error);
+        })
+    }
+
+    useEffect(() =>{
+
+    getAllEmployees();
+
+    },[])
+
+    function addNewEmployee(){
+        navigator('/add-employee')
+    }
+
+    function updateEmployee(id){
+        navigator(`/edit-employee/${id}`)
+    }
+
+    function removeEmployee(id){
+        console.log(id);
+
+        deleteEmployee(id).then((response) => {
+            getAllEmployees();
+        }).catch(error => {
+            console.error(error);
+        })
+
+        //navigator(`/remove-employee/${id}`)
+    }
+
+  return (
+    <div className='container'> 
+
+    <h2 className='text-center'>List of Employees</h2>
+    <button className='btn btn-primary mb-2' onClick={addNewEmployee}>Add Employee</button>
+    <table className='table table-striped table-bordered'>
+        <thead>
+            <tr>
+                <th>Employee ID</th>
+                <th>Employee First Name</th>
+                <th>Employee Last Name</th>
+                <th>Employee Email</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            {
+                employees.map(employee =>
+                    <tr key={employee.id}>
+                        <td>{employee.id}</td>
+                        <td>{employee.firstName}</td>
+                        <td>{employee.lastName}</td>
+                        <td>{employee.email}</td>
+                        <td>
+                            <button className='btn btn-info' onClick={() => updateEmployee(employee.id)}>Update</button>
+                            <button className='btn btn-danger' onClick={() => removeEmployee(employee.id)}
+                                style={{marginLeft: '10px' }}
+                                >Delete</button>
+                        </td>
+                    </tr>
+                )
+            }
+
+        </tbody>
+
+    </table>
+    </div>
+  )
+}
+
+export default ListEmployeeComponent
